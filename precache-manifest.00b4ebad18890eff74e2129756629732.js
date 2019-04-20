7ba9a1a04897d4d9d5bcee0a97a556ee0d307e81
Rob Snyder <rksnyder7@gmail.com>
2019-04-20 09:24:30 -0700

    Updates

---
+++ precache-manifest.00b4ebad18890eff74e2129756629732.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d2079f1bcd518c5abf697119284854f",
+    "url": "/SalaryCards/static/media/packersSunburst2017.6d2079f1.csv"
+  },
+  {
+    "revision": "f4a3d0e57b1cf9df461b",
+    "url": "/SalaryCards/static/css/main.255c2788.chunk.css"
+  },
+  {
+    "revision": "ab07461043b91ad09e77",
+    "url": "/SalaryCards/static/js/runtime~main.a8ef896c.js"
+  },
+  {
+    "revision": "287d413aa2529b0c109748ab686e14b3",
+    "url": "/SalaryCards/static/media/chargers.287d413a.csv"
+  },
+  {
+    "revision": "aa6071e428d0102b53f2",
+    "url": "/SalaryCards/static/js/2.5a8afb21.chunk.js"
+  },
+  {
+    "revision": "f4a3d0e57b1cf9df461b",
+    "url": "/SalaryCards/static/js/main.a16bb219.chunk.js"
+  },
+  {
+    "revision": "e471ad70939ded7091ce1bd32527d9d7",
+    "url": "/SalaryCards/static/media/packersSunburst2018.e471ad70.csv"
+  },
+  {
+    "revision": "82f61d91759fc187719b0184999334c2",
+    "url": "/SalaryCards/static/media/packers.82f61d91.csv"
+  },
+  {
+    "revision": "8e73b15519ec8c4f75f29a332e1d9cc6",
+    "url": "/SalaryCards/static/media/bears.8e73b155.csv"
+  },
+  {
+    "revision": "aa6071e428d0102b53f2",
+    "url": "/SalaryCards/static/css/2.2e98e41a.chunk.css"
+  },
+  {
+    "revision": "dc7b821a871f6367325bc90fc5dd958d",
+    "url": "/SalaryCards/index.html"
+  }
+];
