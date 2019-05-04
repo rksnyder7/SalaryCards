36b6a2201012b57d85d0f9e8c85e9e59a628703e
Rob Snyder <rksnyder7@gmail.com>
2019-05-04 11:13:06 -0700

    updates

--- src/Chart.js
+++ src/Chart.js
@@ -10,12 +10,13 @@ class Chart extends Component {
 		this.createChart();
 	}
 
+
 	createChart() {
 		  var svg = d3.select("svg"),
         margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
 
     // The scale spacing the groups:
     var x0 = d3.scaleBand()
@@ -203,16 +204,19 @@ class Chart extends Component {
                 .duration(100);
 
 
+
         }
 
     });
 	}
 
 	render() {
+        
 		return (
 			<div>
                 <div id="container" className="svg-container">
-    				<svg  width="900" height="500"></svg>
+    				<svg className="svg-content" width="900" height="500">
+                    </svg>
                 </div>
                 <a onClick={this.props.onBack} className="col-xs-1 pt3 black b navbar-left">Back</a>
             </div>
@@ -221,4 +225,3 @@ class Chart extends Component {
 }
 
 export default Chart;
-// width="900" height="500"
